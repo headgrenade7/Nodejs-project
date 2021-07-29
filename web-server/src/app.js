df593469400ba970fac40a4ece6800430a970eb6
CedricT <cedric1204tse@gmail.com>
2021-07-29 18:10:06 +0800

    Update to Session 8

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,62 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
+//Define paths for Express config
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars enginee and vies location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
 
 app.get('', (req, res) => {
-    res.send('Hello Express')
-})
-
-app.get('/help', (req, res) => {
-    res.send('Help Page')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'name'
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About this page</h1>')
+    res.render('about', {
+        title: 'About Node js',
+        name: 'Node js'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        content: 'May find some help',
+        name: 'Cedric'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather')
+    res.send('<h1>Weather</h1>')
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        title: '404',
+        content: 'Help article not found',
+        name: 'Error'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        title: '404',
+        content: 'Page not found',
+        name: 'Error'
+    })
 })
 
 app.listen(3000, () => {
