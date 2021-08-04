0bf933611836f8af0251f4745a2515952881b29d
CedricT <cedric1204tse@gmail.com>
2021-08-04 14:23:29 +0800

    Sync til Session 9

---
+++ web-server/public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Client side javascript file is loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const m1 = document.querySelector('#message_1')
+const m2 = document.querySelector('#message_2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    m1.textContent = 'Loading'
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                console.log(data.error)
+                m1.textContent = data.error
+            }
+            else{
+                m1.textContent = data.location
+                //m2.textContent = `Temp is ${data.forecast.temp} and it feels like ${data.forecast.feelslike}`
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
+    })
+})
